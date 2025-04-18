0232b926f4d9d30423623c876d04b983dc4eebc6
Batyrzhan <batyrzhantogaev2002@icloud.com>
2025-04-18 22:50:29 +0500

    Починил логику регистрацией и авторизацией

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import CreatedUser
+from django.contrib.auth.views import LogoutView
+from .views import CreatedUser, CustomLoginView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='LoginPage'),
+    path('login/', CustomLoginView.as_view(), name='LoginPage'),
     path('logout/', LogoutView.as_view(), name='Logout'),
     path('register/', CreatedUser, name='RegisterPage'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
 
 
 def CreatedUser(request):
+    '''
+    Регистрирует юзера используя стандартную форму.
+    После направляет на страницу авторизацией.
+    '''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -12,3 +18,16 @@ def CreatedUser(request):
         form = UserCreationForm()
 
     return render(request, 'registration/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    '''
+    Кастомная версия LoginView для перенаправления на главную
+    страницу после успешной авторизации.
+    '''
+    redirect_authenticated_user = True
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        """Возвращает URL главной страницы после логина."""
+        return reverse('HomePage')
